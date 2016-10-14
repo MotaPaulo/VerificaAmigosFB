43971c60a4f6dc10aed34dcea45d732936c78903
Paulo <motapaulo@live.com>
2016-10-14 11:01:03 -0300

    inserindo

---
+++ app/src/main/java/ufv/verificaamigosfb/MainActivity.java
@@ -0,0 +1,98 @@
+package ufv.verificaamigosfb;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private CallbackManager mCallbackManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        mCallbackManager = CallbackManager.Factory.create();
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "user_friends"));
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMyFriendsRequest(
+                        AccessToken.getCurrentAccessToken(),
+
+
+                        new GraphRequest.GraphJSONArrayCallback() {
+                            @Override
+                            public void onCompleted(JSONArray array, GraphResponse response) {
+                                //os dados estão dentro desse array
+                                System.out.println("jsonArray: " +  array.optString(1));
+
+
+                                System.out.println("GraphResponse "+ response);
+                            }
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,picture{url},posts,devices{os},likes{category,name,id},events{place{location{city}},rsvp_status}");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+}
